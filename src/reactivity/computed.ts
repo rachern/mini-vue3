65269f0a286fbfb4d4d14f4cd33acc48bb17089e
rachern <455485327@qq.com>
2022-04-11 23:13:01 +0800

    添加 computed 注释

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -5,6 +5,7 @@ class ComputedRefImpl {
     private _value: any
     private _effect: ReactiveEffect;
     constructor(getter) {
+        // 当依赖值发生改变时，触发 scheduler 函数，将 _dirty 标志置为 true
         this._effect = new ReactiveEffect(getter, () => {
             if (!this._dirty) {
                 this._dirty = true
@@ -13,6 +14,8 @@ class ComputedRefImpl {
     }
 
     get value() {
+        // 如果 _dirty 标志为 true，说明没有计算属性值，或者计算属性的值发生改变，需要重新执行 getter 函数
+        // 否则不执行 getter 函数，直接返回之前缓存的值
         if (this._dirty) {
             this._dirty = false
             this._value = this._effect.run()
