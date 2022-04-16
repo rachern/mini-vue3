048ee0624ec5e2309dc698b4531f8ae463d44d61
rachern <455485327@qq.com>
2022-04-16 18:22:18 +0800

    实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,20 @@ import { h } from '../../lib/guide-mini-vue3.esm.js'
 
 export const App = {
     render() {
-        return h('div', 'hello, ' + this.msg)
+        return h(
+            'div', 
+            { 
+                id: 'test-div',
+                class: 'red'
+            }, 
+            // string
+            // 'hello, ' + this.msg
+            // array
+            [
+                h('p', { class: 'red' }, 'red-text'),
+                h('p', { class: 'blue' }, 'blue-text')
+            ]
+        )
     },
 
     setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,12 +8,48 @@ export function render(vnode, container) {
 function patch(vnode: any, container: any) {
     // 处理组件
 
-    // TODO
-    // 判断 是不是 element
-    // processElement()
+    if (typeof vnode.type === 'string') {
+        // 判断 是不是 element
+        processElement(vnode, container)
+    } else if (isObject(vnode.type)) {
+        // 判断 是不是 component
+        processComponent(vnode, container)
+    }
+}
 
-    // 判断 是不是 component
-    processComponent(vnode, container)
+// 处理标签元素 element
+function processElement(vnode: any, container: any) {
+    // 挂载（初始化）
+    mountElement(vnode, container)
+    // TODO
+    // 更新（update）
+}
+
+// 挂载 element
+function mountElement(vnode: any, container: any) {
+    const el = document.createElement(vnode.type)
+
+    const { props, children } = vnode
+    // children (string array)
+    if (typeof children === 'string') {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        // vnode
+        mountChildren(children, el)
+    }
+
+    // props
+    for (const key in props) {
+        el.setAttribute(key, props[key])
+    }
+
+    container.append(el)
+}
+
+function mountChildren(children: any[], container: any) {
+    children.forEach(v => {
+        patch(v, container)
+    })
 }
 
 function processComponent(vnode: any, container: any) {
