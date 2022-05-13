593ac4b0ce9b84879168e5d9a49ddfe7cd9ec726
rachern <455485327@qq.com>
2022-05-14 00:40:29 +0800

    实现 transform

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,40 @@
+import { NodeTypes } from "./ast"
+
+export function transform(root: any, options: any) {
+    // 创建全局上下文对象
+    const context = createTransformContext(root, options)
+
+    // 深度优先，递归遍历
+    traverseNode(root, context)
+}
+
+function createTransformContext(root: any, options: any) {
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || []
+    }
+
+    return context
+}
+
+function traverseNode(node: any, context: any) {
+    // 执行外部传入的处理节点的方法
+    const nodeTransforms = context.nodeTransforms
+    for (let i = 0; i < nodeTransforms.length; i++) {
+        const transform = nodeTransforms[i]
+        transform(node)
+    }
+
+    // 递归执行其子节点
+    traverseChildren(node, context)
+}
+
+function traverseChildren(node: any, context: any) {
+    const children = node.children
+    if (children) {
+        for (let i = 0; i < children.length; i++) {
+            const node = children[i]
+            traverseNode(node, context)
+        }
+    }
+}
